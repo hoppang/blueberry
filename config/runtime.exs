121170c3f791e8ea830823c54236d0b45898c502
Bohun Kim <hoppang@gmail.com>
2023-12-23 23:48:43 +0900

    설정 파일 수정

--- config/runtime.exs
+++ config/runtime.exs
@@ -52,19 +52,18 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :blueberry, BlueberryWeb.Endpoint,
-    server: true,
-    url: [host: host, port: 40843, scheme: "https"],
+    url: [host: host],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      ip: {0, 0, 0, 0},
       port: port
     ],
     https: [
+      ip: {0, 0, 0, 0},
       port: 40843,
-      cipher_suit: :strong,
       keyfile: System.get_env("BLUEBERRY_SSL_KEY_PATH"),
       certfile: System.get_env("BLUEBERRY_SSL_CERT_PATH")
     ],
