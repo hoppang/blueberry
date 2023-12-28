31469eac4daeba4fa503822e51f50c028019372a
Bohun Kim <hoppang@gmail.com>
2023-12-29 01:15:38 +0900

    dialyxir 런타임에서만 쓰도록 변경

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule Blueberry.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:dialyxir, "~> 1.3", only: [:dev, :test]}
+      {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false}
     ]
   end
 
