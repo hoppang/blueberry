b20c1e74faa261c76a1ffa2c8ab4da7a9208b623
Bohun Kim <hoppang@gmail.com>
2023-12-29 02:07:51 +0900

    #12 credo 추가

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule Blueberry.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
